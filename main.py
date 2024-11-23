b5fbb6b65d8d68b8850f5e04a5673f70e834ddc5
lucasvsj <lfvansintjan@uc.cl>
2024-11-23 06:30:22 -0300

    feat(main.py): File system upload implemented

--- main.py
+++ main.py
@@ -11,8 +11,9 @@ load_dotenv()
 AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID')
 AWS_ACCESS_KEY_VALUE = os.getenv('AWS_ACCESS_KEY_VALUE')
 S3_BUCKET = 'meayudai-files'
+S3_REGION = 'oregon'
 # Directory to save uploaded files
-UPLOAD_DIR = "uploads"
+UPLOAD_DIR = "data"
 
 os.makedirs(UPLOAD_DIR, exist_ok=True)
 
@@ -41,18 +42,37 @@ async def root():
 
 @app.post("/pauta/")
 async def upload_file(file: UploadFile = File(...)):
-    # Save file
-    file_location = os.path.join(UPLOAD_DIR, file.filename)
-    with open(file_location, "wb") as buffer:
-        buffer.write(await file.read())
-    
-    return {"filename": file.filename, "message": "File uploaded successfully!"}
+    AWS_CLIENT.upload_fileobj(
+            Fileobj=file.file,  # Streamed file-like object
+            Bucket=S3_BUCKET,
+            Key=f'data/{file.filename}',
+            ExtraArgs={"ContentType": file.content_type}  # Optional: preserve MIME type
+        )
+
+    # Construct a public file URL (if your S3 bucket is public)
+    file_url = f"https://{S3_BUCKET}.s3.{S3_REGION}.amazonaws.com/{file.filename}"
+
+    return {
+        "filename": file.filename,
+        "url": file_url,
+        "message": "File uploaded successfully to S3!"
+    }
+
 
 @app.post("/prueba/")
 async def upload_file(file: UploadFile = File(...)):
-    # Save file
-    file_location = os.path.join(UPLOAD_DIR, file.filename)
-    with open(file_location, "wb") as buffer:
-        buffer.write(await file.read())
-    
-    return {"filename": file.filename, "message": "File uploaded successfully!"}
+    AWS_CLIENT.upload_fileobj(
+            Fileobj=file.file,  # Streamed file-like object
+            Bucket=S3_BUCKET,
+            Key=f'data/{file.filename}',
+            ExtraArgs={"ContentType": file.content_type}  # Optional: preserve MIME type
+        )
+
+    # Construct a public file URL (if your S3 bucket is public)
+    file_url = f"https://{S3_BUCKET}.s3.{S3_REGION}.amazonaws.com/{file.filename}"
+
+    return {
+        "filename": file.filename,
+        "url": file_url,
+        "message": "File uploaded successfully to S3!"
+    }
