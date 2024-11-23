b6fd545cf910443a066372edd4d84a10785cc870
lucasvsj <lfvansintjan@uc.cl>
2024-11-23 05:24:03 -0300

    feat(endpoints): Added endpoints to recive files

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, File, UploadFile
 from fastapi.middleware.cors import CORSMiddleware
 
 from boto3 import client
@@ -11,7 +11,10 @@ load_dotenv()
 AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID')
 AWS_ACCESS_KEY_VALUE = os.getenv('AWS_ACCESS_KEY_VALUE')
 S3_BUCKET = 'meayudai-files'
+# Directory to save uploaded files
+UPLOAD_DIR = "uploads"
 
+os.makedirs(UPLOAD_DIR, exist_ok=True)
 
 AWS_CLIENT = client(aws_access_key_id=AWS_ACCESS_KEY_ID, aws_secret_access_key=AWS_ACCESS_KEY_VALUE, service_name='s3')
 
@@ -34,3 +37,22 @@ async def root():
     buckets = AWS_CLIENT.list_buckets()
     AWS_CLIENT.upload_file(f'data/prueba lourdes completa.pdf', S3_BUCKET, f'test/test5.pdf')
     return {"message": f'File should have been uploaded to bucket: {S3_BUCKET}'}
+
+
+@app.post("/pauta/")
+async def upload_file(file: UploadFile = File(...)):
+    # Save file
+    file_location = os.path.join(UPLOAD_DIR, file.filename)
+    with open(file_location, "wb") as buffer:
+        buffer.write(await file.read())
+    
+    return {"filename": file.filename, "message": "File uploaded successfully!"}
+
+@app.post("/prueba/")
+async def upload_file(file: UploadFile = File(...)):
+    # Save file
+    file_location = os.path.join(UPLOAD_DIR, file.filename)
+    with open(file_location, "wb") as buffer:
+        buffer.write(await file.read())
+    
+    return {"filename": file.filename, "message": "File uploaded successfully!"}
