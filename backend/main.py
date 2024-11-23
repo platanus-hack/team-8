e8c486bd4df6c568324ff89c075ba4446b93a284
lucasvsj <lfvansintjan@uc.cl>
2024-11-23 02:58:18 -0300

    feat(meayudai): Stable version reached

--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,18 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from boto3 import client
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID')
+AWS_ACCESS_KEY_VALUE = os.getenv('AWS_ACCESS_KEY_VALUE')
+
+print(AWS_ACCESS_KEY_ID)
+
+AWS_CLIENT = client(aws_access_key_id=AWS_ACCESS_KEY_ID, aws_secret_access_key=AWS_ACCESS_KEY_VALUE, service_name='s3')
 
 app = FastAPI()
 
@@ -17,4 +30,5 @@ app.add_middleware(
 
 @app.get("/")
 async def root():
-    return {"message": "Hello hack-provincia World"}
+    buckets = AWS_CLIENT.list_buckets()
+    return {"hello": f"{buckets['Buckets']}"}
